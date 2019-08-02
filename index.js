b746e9d662e8f881126f12fe7519046358fe9a20
royhyang <yhsunshining@gmail.com>
2019-08-03 00:16:43 +0800

    feat: Use Koa as exmple and add websocket.

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const http = require('http')
+const Koa = require("koa");
+const app = new Koa();
+const port = 3000;
+const server = require("http").Server(app.callback());
+let socketio = require("socket.io")(server);
 
-const server = http.createServer((req, res) => {
-  res.end('Hello, Zoey!!')
-})
+const routers = require("./routes/index");
+app.use(routers.routes());
 
-const port = 3000
-server.listen(port, 'localhost', () => {
-  console.log(`server listening on port ${port}`)
-})
+let ws = socketio.of("/ws");
+ws.on("connect", socket => {
+  console.log("connect, socket id: " + socket.id);
+  let count = 0;
+
+  socket.on("messagePing", data => {
+    count++;
+    socket.emit("messagePong", "receive " + count + " ping(s), response pong");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnect socket id: " + socket.id);
+  });
+});
+
+server.listen(port, "localhost", () => {
+  console.log(`server listening on port ${port}!`);
+});
